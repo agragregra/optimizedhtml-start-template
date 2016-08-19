8f3e735224d5b0ab13fc3244f6dde364fb969b64
Алексей <agragregra@ya.ru>
2016-08-19 20:13:05 +0700

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+var gulp           = require('gulp'),
+		gutil          = require( 'gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		pngquant       = require('imagemin-pngquant'),
+		cache          = require('gulp-cache'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		fileinclude    = require('gulp-file-include'),
+		gulpRemoveHtml = require('gulp-remove-html'),
+		bourbon        = require('node-bourbon'),
+		ftp            = require( 'vinyl-ftp' );
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('sass', ['headersass'], function() {
+	return gulp.src('app/sass/**/*.sass')
+		.pipe(sass({
+			includePaths: bourbon.includePaths
+		}).on('error', sass.logError))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('headersass', function() {
+	return gulp.src('app/header.sass')
+		.pipe(sass({
+			includePaths: bourbon.includePaths
+		}).on('error', sass.logError))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('libs', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		// 'app/libs/magnific-popup/magnific-popup.min.js'
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('watch', ['sass', 'libs', 'browser-sync'], function() {
+	gulp.watch('app/header.sass', ['headersass']);
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('buildhtml', function() {
+  gulp.src(['app/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@'
+    }))
+    .pipe(gulpRemoveHtml())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+
+gulp.task('build', ['removedist', 'buildhtml', 'imagemin', 'sass', 'libs'], function() {
+
+	var buildCss = gulp.src([
+		'app/css/fonts.min.css',
+		'app/css/main.min.css'
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildFiles = gulp.src([
+		'app/.htaccess'
+	]).pipe(gulp.dest('dist'));
+
+	var buildFonts = gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*').pipe(gulp.dest('dist/js'));
+
+});
+
+gulp.task('deploy', ['build'], function() {
+
+	var conn = ftp.create({
+		host:      'hostname.ru',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+	'dist/**',
+	'dist/.htaccess',
+	];
+	return gulp.src( globs, { buffer: false } )
+	.pipe( conn.dest( '/path/to/folder/on/server' ) );
+
+});
+
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
---
+++ readme.md
@@ -0,0 +1,45 @@
+<h1>OptimizedHTML - Start HTML Template</h1>
+
+<p>OptimizedHTML is all-inclusive Start HTML5 template with Bootstrap (grid only), Gulp, Sass, Browsersync, Autoprefixer, Clean-CSS, Uglify, Imagemin, Vinyl-FTP, Bower (libs path) and Bourbon support.</p>
+
+<p>OptimizedHTML Start Template uses the best practices of web development and optimized for Google PageSpeed.</p>
+
+<p>Cross-browser compatibility: IE9+.</p>
+
+<p>The template uses a Sass with <strong>Sass</strong> syntax and project structure with source code in the directory <strong>app/</strong> and production folder <strong>dist/</strong>, that contains ready project with optimized HTML, CSS, JS and images.</p>
+
+<h2>How to use OptimizedHTML</h2>
+
+<ol>
+	<li>Download <strong>optimizedhtml-start-template</strong> from GitHub;</li>
+	<li>Install Node Modules: <strong>npm i</strong>;</li>
+	<li>Run template: <strong>gulp</strong>.</li>
+</ol>
+
+<h2>Gulp tasks:</h2>
+
+<ul>
+	<li><strong>gulp</strong>: run default gulp task (sass, js, watch, browserSync) for web development;</li>
+	<li><strong>build</strong>: build project to <strong>dist</strong> folder (cleanup, image optimize, removing unnecessary files);</li>
+	<li><strong>deploy</strong>: project deployment on the server from <strong>dist</strong> folder via FTP;</li>
+	<li><strong>clearcache</strong>: clear all gulp cache;</li>
+</ul>
+
+<h2>Rules for working with the starting template</h2>
+
+<ol>
+	<li>All HTML files should have similar content to a file <strong>app/index.html</strong>;</li>
+	<li><strong>Template Basic Images Start</strong> comment in app/index.html - all your custom template basic images (og:image for social networking, favicons for a variety of devices);</li>
+	<li><strong>Load Fonts CSS Start</strong> comment in app/index.html: use <strong>loadCSS</strong> function, if the site is located in a subfolder. Use (uncomment) <strong>loadLocalStorageCSS</strong>, if the site is at the root. One of the lines should always be commented out. All fonts are connected in <strong>app/sass/fonts.sass</strong> with Bourbon;</li>
+	<li><strong>Custom Browsers Color Start</strong> comment in app/index.html: set the color of the browser head on a variety of devices;</li>
+	<li><strong>Custom HTML</strong> comment in app/index.html - all your custom HTML;</li>
+	<li><strong>Optimized loading JS Start</strong> comment in app/index.html: loading all scripts;</li>
+	<li>For installing new jQuery library, just run the command "<strong>bower i plugin-name</strong>" in the terminal. Libraries are automatically placed in the folder <strong>app/libs</strong>. Bower must be installed in the system (npm i -g bower). Then place all jQuery libraries paths in the <strong>'libs'</strong> task (gulpfile.js);</li>
+	<li>All custom JS located in <strong>app/js/common.js</strong>;</li>
+	<li>All Sass vars placed in <strong>app/sass/_vars.sass</strong>;</li>
+	<li>All Bootstrap media queries placed in <strong>app/sass/_media.sass</strong>;</li>
+	<li>All jQuery libraries CSS styles placed in <strong>app/sass/_libs.sass</strong>;</li>
+	<li>All basic styles (html, body, fonts, buttons, etc...) placed in <strong>app/sass/_base.sass</strong>;</li>
+	<li>In the file <strong>app/header.sass</strong> should be placed all styless, responsible for displaying the first screen (on the biggest screens) on all pages;</li>
+	<li>Rename <strong>ht.access</strong> to <strong>.htaccess</strong> before place it in your web server. This file contain rules for files caching in web server.</li>
+</ol>
